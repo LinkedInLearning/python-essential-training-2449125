3502f9dc73e8408f5e312811bba34a8043693aab
REMitchell <Ryan.E.Mitchell@gmail.com>
2022-03-10 21:22:44 -0500

    Add chapter 1 and 2 challenge/solution files

---
+++ exercise_files/01_06_solution.py
@@ -0,0 +1,9 @@
+from termcolor import colored
+
+print(colored('~~~RAINBOW~~~', 'red'))
+print(colored('~~~RAINBOW~~~', 'yellow'))
+print(colored('~~~RAINBOW~~~', 'green'))
+print(colored('~~~RAINBOW~~~', 'cyan'))
+print(colored('~~~RAINBOW~~~', 'blue'))
+print(colored('~~~RAINBOW~~~', 'magenta'))
+
