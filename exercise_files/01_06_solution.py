dcaa9168eaf8762ddb02606b3ab9e7beb127479b
REMitchell <Ryan.E.Mitchell@gmail.com>
2022-07-06 18:21:01 -0400

    Update 01_06_solution.py
    Add colorama instructions for windows users

--- exercise_files/01_06_solution.py
+++ exercise_files/01_06_solution.py
@@ -1,5 +1,11 @@
 from termcolor import colored
 
+# Note: If you are on Windows, 
+# you may need to use Colorama (https://pypi.org/project/colorama/)
+
+#import colorama
+#colorama.init()
+
 print(colored('~~~RAINBOW~~~', 'red'))
 print(colored('~~~RAINBOW~~~', 'yellow'))
 print(colored('~~~RAINBOW~~~', 'green'))
