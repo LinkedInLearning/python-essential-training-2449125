d0144c3ab737f633af95df54ce34f9adcb8e2991
derailed-dash <derailed.dash@gmail.com>
2023-01-08 23:44:55 +0000

    Add Vector Enum

--- exercise_files/02_07_challenge.py
+++ exercise_files/02_07_challenge.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import time
 from termcolor import colored
@@ -8,91 +9,67 @@ class Canvas:
     def __init__(self, width, height):
         self._x = width
         self._y = height
-        # This is a grid that contains data about where the 
-        # TerminalScribes have visited
         self._canvas = [[' ' for y in range(self._y)] for x in range(self._x)]
 
-    # Returns True if the given point is outside the boundaries of the Canvas
-    def hitsWall(self, point):
+    def hits_wall(self, point):
+        """" Returns True if the given point is outside the boundaries of the Canvas """
         return point[0] < 0 or point[0] >= self._x or point[1] < 0 or point[1] >= self._y
 
-    # Set the given position to the provided character on the canvas
-    def setPos(self, pos, mark):
+    def set_pos(self, pos, mark):
+        """ Set the (x, y) position to the provided character on the canvas. """
         self._canvas[pos[0]][pos[1]] = mark
 
-    # Clear the terminal (used to create animation)
     def clear(self):
+        """ Clear the terminal (used to create animation) """
         os.system('cls' if os.name == 'nt' else 'clear')
-
-    # Clear the terminal and then print each line in the canvas
+        
     def print(self):
+        """ Clear the terminal and then print each line in the canvas """
         self.clear()
         for y in range(self._y):
             print(' '.join([col[y] for col in self._canvas]))
 
+class Vector(Enum):
+    UP = (0, -1)
+    RIGHT = (1, 0)
+    DOWN = (0, 1)
+    LEFT = (-1, 0)
+
 class TerminalScribe:
-    def __init__(self, canvas):
+    TRAIL = '.'
+    MARK = '*'
+
+    def __init__(self, canvas, framerate=10):
         self.canvas = canvas
-        self.trail = '.'
-        self.mark = '*'
-        self.framerate = 0.2
+        self.framerate = framerate
         self.pos = [0, 0]
 
-    def up(self):
-        pos = [self.pos[0], self.pos[1]-1]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
-
-    def down(self):
-        pos = [self.pos[0], self.pos[1]+1]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
-
-    def right(self):
-        pos = [self.pos[0]+1, self.pos[1]]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
-
-    def left(self):
-        pos = [self.pos[0]-1, self.pos[1]]
-        if not self.canvas.hitsWall(pos):
+    def move(self, direction: Vector):
+        pos = [self.pos[0]+direction.value[0], self.pos[1]+direction.value[1]]
+        if not self.canvas.hits_wall(pos):
             self.draw(pos)
 
     def draw(self, pos):
         # Set the old position to the "trail" symbol
-        self.canvas.setPos(self.pos, self.trail)
+        self.canvas.set_pos(self.pos, TerminalScribe.TRAIL)
         # Update position
         self.pos = pos
         # Set the new position to the "mark" symbol
-        self.canvas.setPos(self.pos, colored(self.mark, 'red'))
+        self.canvas.set_pos(self.pos, colored(TerminalScribe.MARK, 'red'))
         # Print everything to the screen
         self.canvas.print()
         # Sleep for a little bit to create the animation
-        time.sleep(self.framerate)
+        time.sleep(1/self.framerate)
 
-def draw_square(edge_len: int):
-    for func in [scribe.right, scribe.down, scribe.left, scribe.up]:
-        for _ in range(edge_len):
-            func()
+    def draw_square(self, edge_len: int):
+        for direction in (Vector.RIGHT, Vector.DOWN, Vector.LEFT, Vector.UP):
+            for _ in range(edge_len):
+                self.move(direction)
 
-# Create a new Canvas instance that is 30 units wide by 30 units tall 
-canvas = Canvas(30, 30)
+# Create a new Canvas instance that is 30 units wide by 30 units tall
+my_canvas = Canvas(30, 30)
 
 # Create a new scribe and give it the Canvas object
-scribe = TerminalScribe(canvas)
+scribe = TerminalScribe(my_canvas)
 
-# Draw a small square
-scribe.right()
-scribe.right()
-scribe.right()
-scribe.down()
-scribe.down()
-scribe.down()
-scribe.left()
-scribe.left()
-scribe.left()
-scribe.up()
-scribe.up()
-scribe.up()
-
-draw_square(10)
+scribe.draw_square(10)
